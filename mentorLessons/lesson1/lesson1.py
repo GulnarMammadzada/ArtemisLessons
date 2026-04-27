fdd2b4ca235691f0e7a20fa3c85a7f7ac8f11a42
Gulnar Mammadzada <gulnarmmmdzad05@gmail.com>
2026-04-27 23:01:26 +0400

    Initial commit

---
+++ mentorLessons/lesson1/lesson1.py
@@ -0,0 +1,84 @@
+
+#string
+#integer
+#float
+#boolean
+
+#data_type variable value
+#variable value
+
+# name="Gulnar"
+# yashimiz=20
+# qiymet=5.55
+# kecmek_olarmi=True
+#
+#
+# print("Istifadecin adi",name," \nyasi",yashimiz)
+# print(f" Adimiz {name} Yashimiz {yashimiz} ")
+#
+
+# ad="Gulnar"
+# _ad="GUlnar"
+# menimAdim
+# menim_adim
+# ad3
+
+
+
+# a=2
+# b=3
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a**b
+# print(a+b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+# a=-20
+# b=3
+# c=a/b
+# d=a*b
+# print(c)
+# print(round(c,5))
+# print(d)
+# print(abs(d))
+
+
+
+#and  iki sert,sag ve sol,1i duz 1i sehv- false,2si de sehv-false,her ikisi duz-true
+#or   iki sert,sag duz sol sehv - true,her ikisi duz-true,her ikisi sehv-false
+
+
+# imtahan_bali=85.3
+# davamiyyet_bali=75.8767
+#
+# imtahan_bali=int(input("Imtahan balinizi daxil edin"))
+#
+#
+# if imtahan_bali>90:
+#     print("Elachi")
+# elif imtahan_bali>80:
+#     print("Zerbechi")
+# elif imtahan_bali>70:
+#     print("adi")
+# else :
+#     print("kesilmisiz")
+
+
+# a=10
+# b=5
+# print(a>=b)
+
+
+
+# a=10
+#
+# if a<=15:
+#     print("Siz Elachisiniz")
+# elif a>5:
+#     print("Sizin baliniz 5den yuxaridir")
+# else:
+#     print("Sizin baliniz asaghidir")
